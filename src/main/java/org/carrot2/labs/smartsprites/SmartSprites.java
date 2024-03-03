8e72c4a039217f0d3fa3d2db401a96ac8ff7afdc
Jeremy Landis <jeremylandis@hotmail.com>
2024-03-03 15:53:41 -0500

    [ci] Use url to w3 about smartsprites as original domain taken over by gambling site

--- src/main/java/org/carrot2/labs/smartsprites/SmartSprites.java
+++ src/main/java/org/carrot2/labs/smartsprites/SmartSprites.java
@@ -94,6 +94,6 @@ public class SmartSprites {
         System.out.print("Usage: smartsprites");
         parser.printSingleLineUsage(System.out);
         System.out.println();
-        System.out.println("\nPlease see http://csssprites.org for detailed option descriptions.");
+        System.out.println("\nPlease see https://www.w3schools.com/css/css_image_sprites.asp for detailed option descriptions.");
     }
 }
