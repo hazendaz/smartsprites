c9ec885f0e9c0499fd0b5a69b059297351d70112
Jeremy Landis <jeremylandis@hotmail.com>
2025-12-07 15:45:39 -0500

    Site checkin for project smartsprites

--- testapidocs/package-search-index.js
+++ testapidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.carrot2.labs.smartsprites"},{"l":"org.carrot2.labs.smartsprites.css"},{"l":"org.carrot2.labs.test"},{"l":"org.carrot2.util"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"org.carrot2.labs.smartsprites"},{"l":"org.carrot2.labs.test"},{"l":"org.carrot2.util"}];updateSearchResults();
